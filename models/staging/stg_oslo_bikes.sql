c20af418b56e89ea478c132fefde33ff0cb2c4df
Anna <118570784+makielaa@users.noreply.github.com>
2026-05-31 19:47:33 +0200

    Add files via upload

--- models/staging/stg_oslo_bikes.sql
+++ models/staging/stg_oslo_bikes.sql
@@ -1,18 +1,17 @@
-select
-    try_to_timestamp(col1) as started_at,
-    try_to_timestamp(col2) as ended_at,
-    try_to_number(col3) as duration,
-    try_to_number(col4) as start_station_id,
-    col5 as start_station_name,
-    col6 as start_station_description,
-    try_to_double(col7) as start_station_latitude,
-    try_to_double(col8) as start_station_longitude,
-    try_to_number(col9) as end_station_id,
-    col10 as end_station_name,
-    col11 as end_station_description,
-    try_to_double(col12) as end_station_latitude,
-    try_to_double(col13) as end_station_longitude,
-    added_at,
+SELECT
+    started_at,
+    ended_at,
+    duration,
+    start_station_id,
+    start_station_name,
+    start_station_description,
+    start_station_latitude,
+    start_station_longitude,
+    end_station_id,
+    end_station_name,
+    end_station_description,
+    end_station_latitude,
+    end_station_longitude,
     current_timestamp() as processed_at
+FROM {{ source('raw', 'bikes_stations') }}
 
-FROM {{ source('raw', 'oslo_bikes_raw') }}
