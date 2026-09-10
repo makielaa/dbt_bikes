747518b6deb087f929e5c5e79e851fdf3bf3b85e
Anna Makieła <makiela.ania@gmail.com>
2026-09-10 11:22:43 +0000

    join distinct station_id

--- models/marts/mart_station_availability.sql
+++ models/marts/mart_station_availability.sql
@@ -1,7 +1,15 @@
+-- models/marts/mart_station_availability.sql
+
+{{
+    config(
+        materialized='incremental',
+        unique_key='station_id || snapshot_at'
+    )
+}}
 
 select
     s.station_id,
-    st.start_station_name as station_name,
+    st.station_name,
     s.bikes_available,
     s.docks_available,
     s.is_installed,
@@ -11,5 +19,19 @@ select
     s.snapshot_at,
     date(s.snapshot_at) as snapshot_date
 from {{ ref('stg_station_status') }} s
-left join {{ ref('mart_oslo_bikes') }} st
-    on s.station_id = st.start_station_id
+left join (
+    select distinct
+        start_station_id as station_id,
+        start_station_name as station_name
+    from {{ ref('mart_oslo_bikes') }}
+) st
+    on s.station_id = st.station_id
+
+{% if is_incremental() %}
+where s.snapshot_at > (select max(snapshot_at) from {{ this }})
+{% endif %}
+
+qualify row_number() over (
+    partition by s.station_id, s.snapshot_at
+    order by s.snapshot_at desc
+) = 1
